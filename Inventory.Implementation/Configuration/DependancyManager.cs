0e16eb96e19dc69c207df7759bdaa54d8e13cb54
Rafiq Mattoo <Rafiq.Mattoo@sirva.com>
2022-04-21 01:27:01 +0530

    Add project files.

---
+++ Inventory.Implementation/Configuration/DependancyManager.cs
@@ -0,0 +1,20 @@
+﻿using Inventory.DataAccess;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Implementation.Configuration
+{
+    public class DependancyManager
+    {
+        public void InjectDependencies(IServiceCollection services)
+        {
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+        }
+    }
+}
---
+++ Inventory.Implementation/Services/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿using Inventory.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Implementation
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public  string GetCategoryList()
+        {
+            string result = _categoryRepository.GetCategoriesList();
+            return result;
+        }
+    }
+}
